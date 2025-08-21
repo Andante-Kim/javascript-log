5f9c8a46892cf51dabea263780af05391102881e
MinJeong Kim <Andante.mjkim@gmail.com>
2025-08-21 14:50:06 +0900

    3. Arithemetic operators
    3강 완료

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 // arithmetic operators = operands (values, variables, etc.)
 //                        operators (+ - * /)
 //                        ex.11 = x + 5;
+
+let students = 30;
+
+//students = students + 1;
+//students = students - 1;
+//students = students * 2;
+//students = students / 2;
+//students = students ** 3; //exponent operators (지수 연산자)
+//students = students % 2; //remainder 나머지 나머지modulus 연산자
+
+//students += 1;
+//students -= 1;
+//students *= 2;
+//students /= 2;
+//students **= 2;
+//students %= 2;
+
+//students ++;
+//students --;
+
+//console.log(students);
+
+/*
+    operator precedence
+    1. parenthesis ()
+    2. exponents 
+    3. multiplication & division & modulo
+    4. addition & subtraction
+*/
+
+let result = 1 + 2 * 3 + 4 ** 2;
+//let result = 12 % 5 + 8 / 2;
+//let result = 6 / 2 ** (2 + 5)
+
+console.log(result);
